75cb5a020f198be6848276a193c8b1f4b934bb92
masoumisaipa <masoumisaipa@gmail.com>
2023-02-05 17:22:45 +0330

    addde models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Home(models.Model):
+    title = models.CharField(max_length=255)
+    subTitle = models.CharField(max_length=255)
+    description = models.TextField()
+    subDescription = models.TextField()
+    footerTitle = models.TextField()
+    backgroundVideo = models.FileField()
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    images = models.ImageField(upload_to='media')
